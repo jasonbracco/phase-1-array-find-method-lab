0e64c8a0a4cbe0e6f2f3e07711b7480b7a1f5b1b
Jason Bracco <bracco.jason@gmail.com>
2022-05-06 11:29:44 -0400

    commit tree ffe4986d2b3621085848ccecc73ffe02c075161b

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// code your solution here
+function superbowlWin(array){
+    for (const element of array){
+        if (element.result === "W"){
+        return element.year
+        }
+    }
+}
+record.find(superbowlWin)
